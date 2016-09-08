9f53004e6443e5bc919d1f0fed4c986e621a347a
Trent Newton <trent99878@gmail.com>
2016-09-09 03:14:21 +1000

    getting rid of surround div

--- sites/all/themes/truclean/templates/field.tpl.php
+++ sites/all/themes/truclean/templates/field.tpl.php
@@ -8,15 +8,13 @@
     <div class="field-items"<?php print $content_attributes; ?>>
 <?php } ?>
     <?php foreach ($items as $delta => $item): ?>
-        <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
-            <?php
-                print render($item);
-                // Add comma if not last item
-                if ($delta < (count($items) - 1)) {
-                    print ',';
-                }
-            ?>
-        </div>
+        <?php
+            print render($item);
+            // Add comma if not last item
+            if ($delta < (count($items) - 1)) {
+                print ',';
+            }
+        ?>
     <?php endforeach; ?>
 <?php global $user;
     // Check to see if $user has the administrator role.
