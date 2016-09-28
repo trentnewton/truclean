7f141e6def17ee396fb6667254db4445b8e05276
Trent Newton <trent99878@gmail.com>
2016-09-29 09:15:49 +1000

    removed metatags and added itemscope to html element

--- sites/all/themes/truclean/templates/html.tpl.php
+++ sites/all/themes/truclean/templates/html.tpl.php
@@ -1,16 +1,9 @@
 <!DOCTYPE html>
-<html class="no-js" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+<html class="no-js" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>" itemscope itemtype="http://schema.org/WebSite">
 	<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php print $head_title; ?></title>
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php global $base_path; print $base_path; ?><?php print $directory; ?>/dist/assets/img/apple-touch-icon.png">
-	<link rel="icon" type="image/png" href="<?php global $base_path; print $base_path; ?><?php print $directory; ?>/dist/assets/img/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="<?php global $base_path; print $base_path; ?><?php print $directory; ?>/dist/assets/img/favicon-16x16.png" sizes="16x16">
-	<link rel="manifest" href="<?php global $base_path; print $base_path; ?><?php print $directory; ?>/dist/assets/img/manifest.json">
-	<link rel="mask-icon" href="<?php global $base_path; print $base_path; ?><?php print $directory; ?>/dist/assets/img/safari-pinned-tab.svg" color="#286bb3">
-	<meta name="theme-color" content="#ffffff">
-	<?php print $styles; ?>
-	<?php print $head; ?>
+		<title><?php print $head_title; ?></title>
+		<?php print $styles; ?>
+		<?php print $head; ?>
 	</head>
 	<body id="top" class="<?php print $classes; ?>" <?php print $attributes;?> itemscope itemtype="http://schema.org/WebPage">
 		<?php include ($directory."/partials/svg.php"); ?>
