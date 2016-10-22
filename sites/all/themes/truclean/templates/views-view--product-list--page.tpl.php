fef0a34a7670d767a117256b9076826f096a4063
Trent Newton <trent99878@gmail.com>
2016-10-22 15:09:40 +1000

    fixing mistake

--- sites/all/themes/truclean/templates/views-view--product-list--page.tpl.php
+++ sites/all/themes/truclean/templates/views-view--product-list--page.tpl.php
@@ -32,16 +32,6 @@
     <?php endif; ?>
   </ul>
 
-  <?php if ($rows): ?>
-    <div class="view-content">
-      <?php print $rows; ?>
-    </div>
-  <?php elseif ($empty): ?>
-    <div class="view-empty">
-      <?php print $empty; ?>
-    </div>
-  <?php endif; ?>
-
   <?php if ($pager): ?>
     <?php print $pager; ?>
   <?php endif; ?>
