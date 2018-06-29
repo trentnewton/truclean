3ec227ead4b1f11f669c91907c45c0375b795848
Trent Newton <trent99878@gmail.com>
2018-06-29 13:34:17 +1000

    updated js

--- sites/all/themes/truclean/src/assets/js/app.js
+++ sites/all/themes/truclean/src/assets/js/app.js
@@ -75,7 +75,7 @@ $(document).ready(function(){
     $('#edit-submitted-name').attr('value',$('input[id*="edit-field-company-name"]').attr('value'));
     $('#edit-submitted-address').attr('value',$('input[id*="thoroughfare"]').attr('value'));
     $('#edit-submitted-city-suburb').attr('value',$('input[id*="edit-field-client-address-und-0-locality"]').attr('value'));
-    $('#edit-submitted-state').attr('value',$('input[id*="administrative_area"] option').attr('selected'));
+    $('#edit-submitted-state').attr('value',$('select[id*="administrative_area"] option').attr('selected'));
     $('#edit-submitted-postcode').attr('value',$('input[id*="postal-code"]').attr('value'));
     $('#edit-submitted-phone').attr('value',$('input[id*="edit-field-phone-number"]').attr('value'));
     $('#edit-submitted-contact').attr('value',$('input[id*="edit-field-client-address-und-0-name-line"]').attr('value'));
