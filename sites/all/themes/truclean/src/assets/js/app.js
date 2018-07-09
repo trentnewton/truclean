20c54c11bb618f4d54b6b7da7dd65900716a4e47
Trent Newton <trent99878@gmail.com>
2018-07-09 21:23:01 +1000

    redit

--- sites/all/themes/truclean/src/assets/js/app.js
+++ sites/all/themes/truclean/src/assets/js/app.js
@@ -72,19 +72,18 @@ $(document).ready(function(){
 
         $('#edit-submitted-email').attr('value',$('input[id*="edit-field-email"]').attr('value'));
         $('#edit-submitted-name').attr('value',$('input[id*="edit-field-company-name"]').attr('value'));
-        $('#edit-submitted-address').attr('value',$('input[id*="thoroughfare"]').attr('value'));
-        $('#edit-submitted-city-suburb').attr('value',$('input[id*="edit-field-client-address-und-0-locality"]').attr('value'));
-        $('#edit-submitted-state').attr('value',$('select[id*="administrative_area"] option:selected').attr('value'));
-        $('#edit-submitted-postcode').attr('value',$('input[id*="postal-code"]').attr('value'));
+        $('#edit-submitted-address').attr('value',$('input[id*="edit-field-address"]').attr('value'));
+        $('#edit-submitted-city-suburb').attr('value',$('input[id*="edit-field-city-suburb"]').attr('value'));
+        $('#edit-submitted-state').attr('value',$('select[id*="edit-field-state"] option:selected').attr('value'));
+        $('#edit-submitted-postcode').attr('value',$('input[id*="edit-field-postcode"]').attr('value'));
         $('#edit-submitted-phone').attr('value',$('input[id*="edit-field-phone-number"]').attr('value'));
-        $('#edit-submitted-contact').attr('value',$('input[id*="edit-field-client-address-und-0-name-line"]').attr('value'));
+        $('#edit-submitted-contact').attr('value',$('input[id*="edit-field-contact-name"]').attr('value'));
         $('#edit-submitted-machine-model').attr('value',$('input[id*="edit-field-machine-model"]').attr('value'));
-      }, 2000 /* time to delay */ );
+      }, 1000 /* time to delay */ );
     }
 
   });
 
-  $('select[id*="administrative_area"] option:selected').parent().addClass('radio-pass');
   $(".form-radio[value='pass']").parent().addClass('radio-pass');
   $(".form-radio[value='satisfactory']").parent().addClass('radio-satisfactory');
   $(".form-radio[value='fail']").parent().addClass('radio-fail');
