21db1de1f3494b2925c7b702264bccbd251154c4
Trent Newton <trent99878@gmail.com>
2018-07-09 19:09:30 +1000

    testing

--- sites/all/themes/truclean/src/assets/js/app.js
+++ sites/all/themes/truclean/src/assets/js/app.js
@@ -74,7 +74,7 @@ $(document).ready(function(){
         $('#edit-submitted-name').attr('value',$('input[id*="edit-field-company-name"]').attr('value'));
         $('#edit-submitted-address').attr('value',$('input[id*="thoroughfare"]').attr('value'));
         $('#edit-submitted-city-suburb').attr('value',$('input[id*="edit-field-client-address-und-0-locality"]').attr('value'));
-        $('#edit-submitted-state').attr('value',$('select[id*="administrative_area"] option').find("[selected='selected']").attr('value'));
+        $('#edit-submitted-state').attr('value',$('select[id*="administrative_area"] option[selected="selected"]').attr('value'));
         $('#edit-submitted-postcode').attr('value',$('input[id*="postal-code"]').attr('value'));
         $('#edit-submitted-phone').attr('value',$('input[id*="edit-field-phone-number"]').attr('value'));
         $('#edit-submitted-contact').attr('value',$('input[id*="edit-field-client-address-und-0-name-line"]').attr('value'));
