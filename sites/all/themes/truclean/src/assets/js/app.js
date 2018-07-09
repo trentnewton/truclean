4043168c72d60426730e82f9aa5a4c3d92ebac1a
Trent Newton <trent99878@gmail.com>
2018-07-09 18:11:25 +1000

    testing

--- sites/all/themes/truclean/src/assets/js/app.js
+++ sites/all/themes/truclean/src/assets/js/app.js
@@ -70,7 +70,19 @@ $(document).ready(function(){
   //   $('#edit-submitted-address').val($('input[id*="thoroughfare"]').val());
   // });
 
-  $('#edit-field-client-und').bind('change', function(){
+  // $('#edit-field-client-und').bind('change', function(){
+  //   $('#edit-submitted-email').attr('value',$('input[id*="edit-field-email"]').attr('value'));
+  //   $('#edit-submitted-name').attr('value',$('input[id*="edit-field-company-name"]').attr('value'));
+  //   $('#edit-submitted-address').attr('value',$('input[id*="thoroughfare"]').attr('value'));
+  //   $('#edit-submitted-city-suburb').attr('value',$('input[id*="edit-field-client-address-und-0-locality"]').attr('value'));
+  //   $('#edit-submitted-state').attr('value',$('select[id*="administrative_area"]').find("[selected]").text());
+  //   $('#edit-submitted-postcode').attr('value',$('input[id*="postal-code"]').attr('value'));
+  //   $('#edit-submitted-phone').attr('value',$('input[id*="edit-field-phone-number"]').attr('value'));
+  //   $('#edit-submitted-contact').attr('value',$('input[id*="edit-field-client-address-und-0-name-line"]').attr('value'));
+  //   $('#edit-submitted-machine-model').attr('value',$('input[id*="edit-field-machine-model"]').attr('value'));
+  // });
+
+  $(document).on('change', '#edit-field-client-und', function(e) {
     $('#edit-submitted-email').attr('value',$('input[id*="edit-field-email"]').attr('value'));
     $('#edit-submitted-name').attr('value',$('input[id*="edit-field-company-name"]').attr('value'));
     $('#edit-submitted-address').attr('value',$('input[id*="thoroughfare"]').attr('value'));
@@ -80,7 +92,7 @@ $(document).ready(function(){
     $('#edit-submitted-phone').attr('value',$('input[id*="edit-field-phone-number"]').attr('value'));
     $('#edit-submitted-contact').attr('value',$('input[id*="edit-field-client-address-und-0-name-line"]').attr('value'));
     $('#edit-submitted-machine-model').attr('value',$('input[id*="edit-field-machine-model"]').attr('value'));
-  });
+});
 
   // $('#edit-field-client-und').on('change', function(){
   //   alert('here');
