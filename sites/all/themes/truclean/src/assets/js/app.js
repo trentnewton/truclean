02e976ea078443532115a3c4cf5452eacf64a7c4
Trent Newton <trent99878@gmail.com>
2018-07-09 18:27:31 +1000

    testing

--- sites/all/themes/truclean/src/assets/js/app.js
+++ sites/all/themes/truclean/src/assets/js/app.js
@@ -31,17 +31,17 @@ $('a[href*=\\#]:not([href=\\#])').click(function () {
 	}
 });
 
-$('#edit-field-client-und').bind('change', function(){
-    $('#edit-submitted-email').attr('value',$('input[id*="edit-field-email"]').attr('value'));
-    $('#edit-submitted-name').attr('value',$('input[id*="edit-field-company-name"]').attr('value'));
-    $('#edit-submitted-address').attr('value',$('input[id*="thoroughfare"]').attr('value'));
-    $('#edit-submitted-city-suburb').attr('value',$('input[id*="edit-field-client-address-und-0-locality"]').attr('value'));
-    $('#edit-submitted-state').attr('value',$('select[id*="administrative_area"]').find("[selected]").text());
-    $('#edit-submitted-postcode').attr('value',$('input[id*="postal-code"]').attr('value'));
-    $('#edit-submitted-phone').attr('value',$('input[id*="edit-field-phone-number"]').attr('value'));
-    $('#edit-submitted-contact').attr('value',$('input[id*="edit-field-client-address-und-0-name-line"]').attr('value'));
-    $('#edit-submitted-machine-model').attr('value',$('input[id*="edit-field-machine-model"]').attr('value'));
-  });
+// $('#edit-field-client-und').on('change', function(){
+//     $('#edit-submitted-email').attr('value',$('input[id*="edit-field-email"]').attr('value'));
+//     $('#edit-submitted-name').attr('value',$('input[id*="edit-field-company-name"]').attr('value'));
+//     $('#edit-submitted-address').attr('value',$('input[id*="thoroughfare"]').attr('value'));
+//     $('#edit-submitted-city-suburb').attr('value',$('input[id*="edit-field-client-address-und-0-locality"]').attr('value'));
+//     $('#edit-submitted-state').attr('value',$('select[id*="administrative_area"]').find("[selected]").text());
+//     $('#edit-submitted-postcode').attr('value',$('input[id*="postal-code"]').attr('value'));
+//     $('#edit-submitted-phone').attr('value',$('input[id*="edit-field-phone-number"]').attr('value'));
+//     $('#edit-submitted-contact').attr('value',$('input[id*="edit-field-client-address-und-0-name-line"]').attr('value'));
+//     $('#edit-submitted-machine-model').attr('value',$('input[id*="edit-field-machine-model"]').attr('value'));
+//   });
 
 $(document).ready(function(){
 
@@ -87,6 +87,14 @@ $(document).ready(function(){
   // $('#edit-field-client-und').on('change', function(){
   //   alert('here');
   // });
+
+  $('#edit-field-client-und').on('change', function(){
+
+    if ( $(this).val() != "_none" ) {
+      alert('It changed!');
+    }
+
+  });
 
   $(".form-radio[value='pass']").parent().addClass('radio-pass');
   $(".form-radio[value='satisfactory']").parent().addClass('radio-satisfactory');
