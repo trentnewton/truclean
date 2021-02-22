c963c8594b5e803fb2c51e753bfe616c68d4da88
Trent Newton <info@trentnewton.com>
2021-02-22 14:27:10 +1000

    Update reveal_login.php

--- sites/all/themes/truclean/partials/reveal_login.php
+++ sites/all/themes/truclean/partials/reveal_login.php
@@ -10,7 +10,8 @@
         <span aria-hidden="true">&times;</span>
     </button> -->
     <script language='javascript' type='text/javascript' src="https://web.salesin.com/Javascript/md5.js"></script>
-    <script language='javascript' type='text/javascript' src="<?php print $base_path .$directory; ?>/LoginControlGeneration.js"></script>
+    <script language='javascript' type='text/javascript' src="https://web.salesin.com/Javascript/LoginControlGeneration.js"></script>
+    <!-- <script language='javascript' type='text/javascript' src="<?php print $base_path .$directory; ?>/LoginControlGeneration.js"></script> -->
     <script language='javascript' type='text/javascript'>
         initLoginSection("https://web.salesin.com/", "0wlgznfoPYllA9Q7oknK9w==", "https://b2b.salesin.com/", "https://auth.salesin.com/");
     </script>
