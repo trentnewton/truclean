44c299c37a0640b253672d8001f28c7f15c41fe5
Trent Newton <info@trentnewton.com>
2021-05-19 10:19:15 +1000

    updated js for blend centre report form

--- sites/all/themes/truclean/src/assets/js/app.js
+++ sites/all/themes/truclean/src/assets/js/app.js
@@ -80,6 +80,12 @@ $(document).ready(function(){
         $('#edit-submitted-phone').attr('value',$('input[id*="edit-field-phone-number"]').attr('value'));
         $('#edit-submitted-contact').attr('value',$('input[id*="edit-field-contact-name"]').attr('value'));
         $('#edit-submitted-machine-model').attr('value',$('input[id*="edit-field-machine-model"]').attr('value'));
+        $('#edit-submitted-blend-centre-location').attr('value',$('input[id*="edit-field-blend-centre-location"]').attr('value'));
+        $('#edit-submitted-blend-centre-make').attr('value',$('input[id*="edit-field-blend-centre-make"]').attr('value'));
+        $('#edit-submitted-blend-centre-model').attr('value',$('input[id*="edit-field-blend-centre-model"]').attr('value'));
+        $('#edit-submitted-blend-centre-serial-number').attr('value',$('input[id*="edit-field-blend-centre-serial-number"]').attr('value'));
+        $('#edit-submitted-call-out-type').attr('value',$('input[id*="edit-field-call-out-type"]').attr('value'));
+        $('#edit-submitted-dispenser-configuration').attr('value',$('input[id*="edit-field-dispenser-configuration"]').attr('value'));
       }, 1000 /* time to delay */ );
     }
 
